ff7aa5bf7938934ecef6637c164874c5dcd51ea7
shivaganesht <shivaganesh9108@gmail.com>
2025-10-13 21:04:15 +0530

    feat: Update UPI payment link for TEDxKPRIT event to include correct parameters

--- components/events/EventRegistrationForm.tsx
+++ components/events/EventRegistrationForm.tsx
@@ -35,7 +35,7 @@ export default function EventRegistrationForm({ event, ticket, onClose }: EventR
   })
 
   // UPI Payment Link for TEDxKPRIT (₹499)
-  const UPI_PAYMENT_LINK = 'upi://pay?pa=Vyapar.171588997321@hdfcbank'
+  const UPI_PAYMENT_LINK = 'upi://pay?pa=bhuvaneshwaripothuraju2005@oksbi&pn=matriXOxTEDxKPRIT&am=499.00&cu=INR'
 
   // Detect mobile device
   useEffect(() => {
